f2c7be311594a209f60ff870db8c4ce9b75aacfd
Tristan Bierig <112637018+TristanBierig@users.noreply.github.com>
2023-07-30 18:40:44 +0200

    image files
    Fire Water Wind

--- js/images.js
+++ js/images.js
@@ -251,6 +251,258 @@ let allImages = {
                 ]
              }
             }
+        },
+        Fire: {
+            normalForm: {
+                movements: {
+                    idle: [
+                        'img/Character/Earth/png/idle/idle_1.png',
+                        'img/Character/Earth/png/idle/idle_2.png',
+                        'img/Character/Earth/png/idle/idle_3.png',
+                        'img/Character/Earth/png/idle/idle_4.png',
+                        'img/Character/Earth/png/idle/idle_5.png',
+                        'img/Character/Earth/png/idle/idle_6.png',
+                    ],
+                    move: [
+                        'img/Character/Earth/png/run/run_1.png',
+                        'img/Character/Earth/png/run/run_2.png',
+                        'img/Character/Earth/png/run/run_3.png',
+                        'img/Character/Earth/png/run/run_4.png',
+                        'img/Character/Earth/png/run/run_5.png',
+                        'img/Character/Earth/png/run/run_6.png',
+                        'img/Character/Earth/png/run/run_7.png',
+                        'img/Character/Earth/png/run/run_8.png'
+                    ],
+                    jumpUp: [
+                        'img/Character/Earth/png/j_up/j_up_1.png',
+                        'img/Character/Earth/png/j_up/j_up_2.png',
+                        'img/Character/Earth/png/j_up/j_up_3.png'
+                    ],
+                    jumpDown: [
+                        'img/Character/Earth/png/j_down/j_down_1.png',
+                        'img/Character/Earth/png/j_down/j_down_2.png',
+                        'img/Character/Earth/png/j_down/j_down_3.png'
+                    ],
+                    takingHit: [
+                        'img/Character/Earth/png/take_hit/take_hit_1.png',
+                        'img/Character/Earth/png/take_hit/take_hit_2.png',
+                        'img/Character/Earth/png/take_hit/take_hit_3.png',
+                        'img/Character/Earth/png/take_hit/take_hit_4.png',
+                        'img/Character/Earth/png/take_hit/take_hit_5.png',
+                        'img/Character/Earth/png/take_hit/take_hit_6.png'
+                    ],
+                    death: [
+                        'img/Character/Earth/png/death/death_1.png',
+                        'img/Character/Earth/png/death/death_2.png',
+                        'img/Character/Earth/png/death/death_3.png',
+                        'img/Character/Earth/png/death/death_4.png',
+                        'img/Character/Earth/png/death/death_5.png',
+                        'img/Character/Earth/png/death/death_6.png',
+                        'img/Character/Earth/png/death/death_7.png',
+                        'img/Character/Earth/png/death/death_8.png',
+                        'img/Character/Earth/png/death/death_9.png',
+                        'img/Character/Earth/png/death/death_10.png',
+                        'img/Character/Earth/png/death/death_11.png',
+                        'img/Character/Earth/png/death/death_12.png',
+                        'img/Character/Earth/png/death/death_13.png',
+                        'img/Character/Earth/png/death/death_14.png',
+                        'img/Character/Earth/png/death/death_15.png',
+                        'img/Character/Earth/png/death/death_16.png',
+                        'img/Character/Earth/png/death/death_17.png',
+                        'img/Character/Earth/png/death/death_18.png'
+                    ]
+                },
+                abilities: {
+                    qAttack: [
+                        'img/Character/Earth/png/1_atk/1_atk_1.png',
+                        'img/Character/Earth/png/1_atk/1_atk_2.png',
+                        'img/Character/Earth/png/1_atk/1_atk_3.png',
+                        'img/Character/Earth/png/1_atk/1_atk_4.png',
+                        'img/Character/Earth/png/1_atk/1_atk_5.png',
+                        'img/Character/Earth/png/1_atk/1_atk_6.png'
+                    ],
+                    qAttackAir: [
+                        'img/Character/Earth/png/air_atk/air_atk_1.png',
+                        'img/Character/Earth/png/air_atk/air_atk_2.png',
+                        'img/Character/Earth/png/air_atk/air_atk_3.png',
+                        'img/Character/Earth/png/air_atk/air_atk_4.png',
+                        'img/Character/Earth/png/air_atk/air_atk_5.png',
+                        'img/Character/Earth/png/air_atk/air_atk_6.png',
+                        'img/Character/Earth/png/air_atk/air_atk_7.png'
+                    ],
+                    wAttack: [
+                        'img/Earthspell/W/tile000.png',
+                        'img/Earthspell/W/tile001.png',
+                        'img/Earthspell/W/tile002.png',
+                        'img/Earthspell/W/tile003.png',
+                        'img/Earthspell/W/tile004.png',
+                        'img/Earthspell/W/tile005.png',
+                        'img/Earthspell/W/tile006.png',
+                        'img/Earthspell/W/tile007.png',
+                        'img/Earthspell/W/tile008.png',
+                        'img/Earthspell/W/tile009.png',
+                        'img/Earthspell/W/tile010.png',
+                        'img/Earthspell/W/tile011.png',
+                        'img/Earthspell/W/tile012.png',
+                        'img/Earthspell/W/tile013.png',
+                        'img/Earthspell/W/tile014.png',
+                        'img/Earthspell/W/tile015.png',
+                        'img/Earthspell/W/tile016.png',
+                        'img/Earthspell/W/tile017.png',
+                        'img/Earthspell/W/tile018.png',
+                        'img/Earthspell/W/tile019.png'
+                    ],
+                    eAttackFlying: [
+                        'img/Earthspell/first/flying/tile013.png',
+                        'img/Earthspell/first/flying/tile014.png',
+                        'img/Earthspell/first/flying/tile015.png',
+                        'img/Earthspell/first/flying/tile016.png',
+                        'img/Earthspell/first/flying/tile017.png',
+                        'img/Earthspell/first/flying/tile018.png',
+                        'img/Earthspell/first/flying/tile019.png',
+                        'img/Earthspell/first/flying/tile020.png',
+                        'img/Earthspell/first/flying/tile021.png',
+                        'img/Earthspell/first/flying/tile022.png'
+                    ],
+                    eAttackHit: [
+                        'img/Earthspell/first/hitting/tile026.png',
+                        'img/Earthspell/first/hitting/tile027.png',
+                        'img/Earthspell/first/hitting/tile028.png',
+                        'img/Earthspell/first/hitting/tile029.png',
+                        'img/Earthspell/first/hitting/tile030.png',
+                        'img/Earthspell/first/hitting/tile031.png',
+                        'img/Earthspell/first/hitting/tile032.png',
+                        'img/Earthspell/first/hitting/tile033.png',
+                        'img/Earthspell/first/hitting/tile034.png'
+                    ]
+                },
+                transform: [
+                    'img/Character/Earth/png/transform/transform_1.png',
+                    'img/Character/Earth/png/transform/transform_2.png',
+                    'img/Character/Earth/png/transform/transform_3.png',
+                    'img/Character/Earth/png/transform/transform_4.png',
+                    'img/Character/Earth/png/transform/transform_5.png',
+                    'img/Character/Earth/png/transform/transform_6.png',
+                    'img/Character/Earth/png/transform/transform_7.png',
+                    'img/Character/Earth/png/transform/transform_8.png',
+                    'img/Character/Earth/png/transform/transform_9.png',
+                    'img/Character/Earth/png/transform/transform_10.png',
+                    'img/Character/Earth/png/transform/transform_11.png',
+                    'img/Character/Earth/png/transform/transform_12.png',
+                    'img/Character/Earth/png/transform/transform_13.png',
+                    'img/Character/Earth/png/transform/transform_14.png',
+                    'img/Character/Earth/png/transform/transform_15.png',
+                    'img/Character/Earth/png/transform/transform_16.png',
+                    'img/Character/Earth/png/transform/transform_17.png',
+                    'img/Character/Earth/png/transform/transform_18.png',
+                    'img/Character/Earth/png/transform/transform_19.png',
+                    'img/Character/Earth/png/transform/transform_20.png',
+                    'img/Character/Earth/png/transform/transform_21.png',
+                    'img/Character/Earth/png/transform/transform_22.png',
+                    'img/Character/Earth/png/transform/transform_23.png',
+                    'img/Character/Earth/png/transform/transform_24.png',
+                    'img/Character/Earth/png/transform/transform_25.png',
+                    'img/Character/Earth/png/transform/transform_26.png',
+                    'img/Character/Earth/png/transform/transform_27.png',
+                    'img/Character/Earth/png/transform/transform_28.png',
+                    'img/Character/Earth/png/transform/transform_29.png',
+                ]
+            },
+            evolvedForm: {
+             movements: {
+                idle: [
+                    'img/Character/Earth/png/e_idle/e_idle_1.png',
+                    'img/Character/Earth/png/e_idle/e_idle_2.png',
+                    'img/Character/Earth/png/e_idle/e_idle_3.png',
+                    'img/Character/Earth/png/e_idle/e_idle_4.png',
+                    'img/Character/Earth/png/e_idle/e_idle_5.png',
+                    'img/Character/Earth/png/e_idle/e_idle_6.png'
+                ],
+                move: [
+                    'img/Character/Earth/png/e_walk/e_walk_1.png',
+                    'img/Character/Earth/png/e_walk/e_walk_2.png',
+                    'img/Character/Earth/png/e_walk/e_walk_3.png',
+                    'img/Character/Earth/png/e_walk/e_walk_4.png',
+                    'img/Character/Earth/png/e_walk/e_walk_5.png',
+                    'img/Character/Earth/png/e_walk/e_walk_6.png',
+                    'img/Character/Earth/png/e_walk/e_walk_7.png',
+                    'img/Character/Earth/png/e_walk/e_walk_8.png',
+                    'img/Character/Earth/png/e_walk/e_walk_9.png',
+                    'img/Character/Earth/png/e_walk/e_walk_10.png',
+                    'img/Character/Earth/png/e_walk/e_walk_11.png',
+                    'img/Character/Earth/png/e_walk/e_walk_12.png'
+                ],
+                jumpUp: [
+                    'img/Character/Earth/png/e_jump/e_jump_3.png',
+                    'img/Character/Earth/png/e_jump/e_jump_4.png',
+                    'img/Character/Earth/png/e_jump/e_jump_5.png'
+                ],
+                jumpDown: [
+                    'img/Character/Earth/png/e_jump/e_jump_7.png',
+                    'img/Character/Earth/png/e_jump/e_jump_6.png',
+                    'img/Character/Earth/png/e_jump/e_jump_8.png'
+                ],
+                takingHit: [
+                    'img/Character/Earth/png/e_take_hit/e_take_hit_1.png',
+                    'img/Character/Earth/png/e_take_hit/e_take_hit_2.png',
+                    'img/Character/Earth/png/e_take_hit/e_take_hit_3.png',
+                    'img/Character/Earth/png/e_take_hit/e_take_hit_4.png',
+                    'img/Character/Earth/png/e_take_hit/e_take_hit_5.png',
+                    'img/Character/Earth/png/e_take_hit/e_take_hit_6.png'
+                ],
+                death: [
+                    'img/Character/Earth/png/back2human/back2human_1.png',
+                    'img/Character/Earth/png/back2human/back2human_2.png',
+                    'img/Character/Earth/png/back2human/back2human_3.png',
+                    'img/Character/Earth/png/back2human/back2human_4.png',
+                    'img/Character/Earth/png/back2human/back2human_5.png',
+                    'img/Character/Earth/png/back2human/back2human_6.png',
+                    'img/Character/Earth/png/back2human/back2human_7.png',
+                    'img/Character/Earth/png/back2human/back2human_8.png',
+                    'img/Character/Earth/png/back2human/back2human_9.png',
+                    'img/Character/Earth/png/back2human/back2human_10.png',
+                    'img/Character/Earth/png/back2human/back2human_11.png',
+                    'img/Character/Earth/png/death/death_1.png',
+                    'img/Character/Earth/png/death/death_2.png',
+                    'img/Character/Earth/png/death/death_3.png',
+                    'img/Character/Earth/png/death/death_4.png',
+                    'img/Character/Earth/png/death/death_5.png',
+                    'img/Character/Earth/png/death/death_6.png',
+                    'img/Character/Earth/png/death/death_7.png',
+                    'img/Character/Earth/png/death/death_8.png',
+                    'img/Character/Earth/png/death/death_9.png',
+                    'img/Character/Earth/png/death/death_10.png',
+                    'img/Character/Earth/png/death/death_11.png',
+                    'img/Character/Earth/png/death/death_12.png',
+                    'img/Character/Earth/png/death/death_13.png',
+                    'img/Character/Earth/png/death/death_14.png',
+                    'img/Character/Earth/png/death/death_15.png',
+                    'img/Character/Earth/png/death/death_16.png',
+                    'img/Character/Earth/png/death/death_17.png',
+                    'img/Character/Earth/png/death/death_18.png'
+                ]
+             },
+             abilities: {
+                qAttack: [
+                    'img/Character/Earth/png/e_1_atk/e_1_atk_1.png',
+                    'img/Character/Earth/png/e_1_atk/e_1_atk_2.png',
+                    'img/Character/Earth/png/e_1_atk/e_1_atk_3.png',
+                    'img/Character/Earth/png/e_1_atk/e_1_atk_4.png',
+                    'img/Character/Earth/png/e_1_atk/e_1_atk_5.png',
+                    'img/Character/Earth/png/e_1_atk/e_1_atk_6.png',
+                    'img/Character/Earth/png/e_1_atk/e_1_atk_7.png'
+                ],
+                qAttackAir: [
+                    'img/Character/Earth/png/e_air_atk/e_air_atk_1.png',
+                    'img/Character/Earth/png/e_air_atk/e_air_atk_2.png',
+                    'img/Character/Earth/png/e_air_atk/e_air_atk_3.png',
+                    'img/Character/Earth/png/e_air_atk/e_air_atk_4.png',
+                    'img/Character/Earth/png/e_air_atk/e_air_atk_5.png',
+                    'img/Character/Earth/png/e_air_atk/e_air_atk_6.png',
+                    'img/Character/Earth/png/e_air_atk/e_air_atk_7.png'
+                ]
+             }
+            }
         }
     },
     enemies: {
